23a8d31229258a6394a16075222bc666bef27786
csqrl <21029087+csqrl@users.noreply.github.com>
2022-04-27 17:22:50 +0100

    Dictionary: entries, update, values

---
+++ src/Dictionary/Update.lua
@@ -0,0 +1,61 @@
+--!strict
+local Sift = script.Parent.Parent
+
+local Util = require(Sift.Util)
+local copy = require(script.Parent.Copy)
+
+type Callback<K, V> = (key: K) -> V
+type Updater<K, V> = (value: V, key: K) -> V
+
+local function call<K, V>(callback: Callback<K, V>, key: K)
+	if type(callback) == "function" then
+		return callback(key)
+	end
+end
+
+--[=[
+  @function Update
+  @within Dictionary
+
+  @param dictionary {[K]: V} -- The dictionary to update.
+  @param key K -- The key to update.
+  @param updater? (value: V, key: K) -> V -- The updater function.
+  @param callback? (key: K) -> V -- The callback function.
+  @return {[K]: V} -- The updated dictionary.
+
+  Updates a value in a dictionary at the given key. If the value at the given key does not exist, `callback` will be called, and its return value will be used as the value at the given key.
+
+  ```lua
+  local dictionary = { cats = 2 }
+
+  local new = Update(dictionary, "cats", function(value)
+    return value + 1
+  end) -- { cats = 3 }
+
+  local new = Update(dictionary, "dogs", function(value)
+    return value + 1
+  end, function(value)
+    return 1
+  end) -- { cats = 3, dogs = 1 }
+  ```
+]=]
+local function update<K, V>(
+	dictionary: { [K]: V },
+	key: K,
+	updater: Updater<K, V>?,
+	callback: Callback<K, V>?
+): { [K]: V }
+	local result = copy(dictionary)
+
+	updater = if type(updater) == "function" then updater else Util.Func.Returned
+
+	if result[key] ~= nil then
+		result[key] = updater(result[key], key)
+	else
+		result[key] = call(callback, key)
+	end
+
+	return result
+end
+
+return update
