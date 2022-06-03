bdabfd21f3a764536c4cf095c1d2945e474b1393
csqrl <21029087+csqrl@users.noreply.github.com>
2022-06-03 19:57:26 +0100

    Update insert.lua
    Fix indentation because moonwave is fussy.

--- src/Array/insert.lua
+++ src/Array/insert.lua
@@ -1,22 +1,22 @@
 --!strict
 --[=[
-  @function insert
-  @within Array
+	@function insert
+	@within Array
 
-  @param array {T} -- The array to insert the value into.
-  @param index number -- The index to insert the value at (can be negative).
-  @param values ...T -- The values to insert.
-  @return {T} -- The array with the value inserted.
+	@param array {T} -- The array to insert the value into.
+	@param index number -- The index to insert the value at (can be negative).
+	@param values ...T -- The values to insert.
+	@return {T} -- The array with the value inserted.
 
-  Inserts the given values into an array at the given index, shifting all values after it to the right. If the index is negative (or 0), it is counted from the end of the array.
+	Inserts the given values into an array at the given index, shifting all values after it to the right. If the index is negative (or 0), it is counted from the end of the array.
 
 	If the index to insert at is out of range, the array is not modified.
 
-  ```lua
-  local array = { 1, 2, 3 }
+	```lua
+	local array = { 1, 2, 3 }
 
-  local newArray = Insert(array, 2, 4) -- { 1, 4, 2, 3 }
-  ```
+	local newArray = Insert(array, 2, 4) -- { 1, 4, 2, 3 }
+	```
 ]=]
 local function insert<T>(array: { T }, index: number, ...: T): { T }
 	local length = #array
