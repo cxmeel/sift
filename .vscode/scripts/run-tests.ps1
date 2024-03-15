02a849a35cdd018551731f6cbf534c7a32aae379
Cam <21029087+cxmeel@users.noreply.github.com>
2024-03-15 13:25:30 +0000

    Update editor configs

--- .vscode/scripts/run-tests.ps1
+++
@@ -1,8 +0,0 @@
-$FILE_NAME = ".\test-place.rbxl"
-$TEST_PROJ = ".\test.project.json"
-$TEST_RUNR = ".\TestRunner.server.lua"
-
-aftman install
-rojo build "$TEST_PROJ" -o "$FILE_NAME"
-run-in-roblox --place "$FILE_NAME" --script "$TEST_RUNR"
-Remove-Item "$FILE_NAME" -Force -ErrorAction SilentlyContinue
