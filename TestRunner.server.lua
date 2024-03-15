a76e47e9ed3839d2c4ad0d3ba39848ecf373269b
Cam <21029087+cxmeel@users.noreply.github.com>
2024-03-15 13:25:53 +0000

    Remove testez

--- TestRunner.server.lua
+++
@@ -1,8 +0,0 @@
-local ServerScripts = game:GetService("ServerScriptService")
-local TestService = game:GetService("TestService")
-
-local TestEZ = require(TestService.Packages.TestEZ)
-
-TestEZ.TestBootstrap:run({
-	ServerScripts.Sift,
-})
