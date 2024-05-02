139e24a888233bf587551c688f8fe548ff14e681
Kevin <kevin@unsquared.com>
2024-05-02 05:12:53 -0500

    Fix Dictionary.update type (#22)
    Co-authored-by: cxmeel <21029087+cxmeel@users.noreply.github.com>

--- src/Dictionary/update.lua
+++ src/Dictionary/update.lua
@@ -1,24 +1,15 @@
 --!strict
 local copy = require(script.Parent.copy)
 
-type Callback<K, V> = (key: K) -> V
-type Updater<K, V> = (value: V, key: K) -> V
-
-local function call<K, V>(callback: Callback<K, V>, key: K)
-	if type(callback) == "function" then
-		return callback(key)
-	end
-end
-
 --[=[
   @function update
   @within Dictionary
 
-  @param dictionary {[K]: V} -- The dictionary to update.
+  @param dictionary {[K]: V?} -- The dictionary to update.
   @param key K -- The key to update.
   @param updater? (value: V, key: K) -> U -- The updater function.
   @param callback? (key: K) -> C -- The callback function.
-  @return {[K]: V & U & C } -- The updated dictionary.
+  @return {[K]: V | U | C} -- The updated dictionary.
 
   Updates a value in a dictionary at the given key. If the value at the given key does not exist, `callback` will be called, and its return value will be used as the value at the given key.
 
@@ -37,20 +28,20 @@ end
   ```
 ]=]
 local function update<K, V, U, C>(
-	dictionary: { [K]: V },
+	dictionary: { [K]: V? },
 	key: K,
 	updater: ((value: V, key: K) -> U)?,
 	callback: ((key: K) -> C)?
-): { [K]: V & U & C }
-	local result = copy(dictionary)
+): { [K]: V | U | C }
+	local result: { [K]: any } = copy(dictionary)
 
-	if result[key] ~= nil then
+	if result[key] then
 		if updater then
 			result[key] = updater(result[key], key)
 		end
 	else
-		if callback then
-			result[key] = call(callback, key)
+		if typeof(callback) == "function" then
+			result[key] = callback(key)
 		end
 	end
 
